e8cf92a14adc926367e943b8529b4b2fa79f3b5f
frallan97 <franssjos@gmail.com>
2020-12-01 13:36:55 +0100

    created .txt files and a python sulution including tempty test file

---
+++ frans_unittest.py
@@ -0,0 +1,13 @@
+
+import search_frans    # The code to test
+import unittest   # The test framework
+
+class Test_Search_frans(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(4, 4)
+
+    def test_decrement(self):
+        self.assertEqual(2, 2)
+
+if __name__ == '__main__':
+    unittest.main()
